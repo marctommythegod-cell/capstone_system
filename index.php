b83e93fb868fcae1a0aa84ac5e577169b7722ffa
Marc Tommy <marctommythegod@gmail.com>
2026-04-03 20:52:45 +0800

    log in page

--- index.php
+++ index.php
@@ -119,10 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                     <!-- Password Field -->
                     <div class="form-group">
-                        <div class="label-row">
-                            <label for="password" class="form-label">Password</label>
-                            <a href="#forgot-password" class="forgot-link">Forgot Password?</a>
-                        </div>
+                        <label for="password" class="form-label">Password</label>
                         <div class="input-wrapper">
                             <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                             <input 
@@ -139,6 +136,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                 <svg class="eye-icon eye-hide" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" style="display:none"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
                             </button>
                         </div>
+                        <a href="#forgot-password" class="forgot-link">Forgot Password?</a>
                     </div>
 
                     <!-- Remember Me Checkbox -->
